7ae25cdf0aa73d2e6a84c85558c75d9dab7cc67f
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2013-09-26 10:55:09 +0200

    cron configuration added

--- thif_import.py
+++ thif_import.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from argparse import ArgumentParser
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer
-from sqlalchemy.dialects.mysql import VARCHAR, TINYINT, DATETIME
-from sqlalchemy.orm import sessionmaker, mapper
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.dialects.mysql import VARCHAR, TINYINT, DATE, BIGINT
+from sqlalchemy.orm import sessionmaker
+from crontab import CronTab
 import json
 import glob
 import os
@@ -30,25 +33,32 @@ CSV_DB_MAPPER = {
     'enp': 'UPN',
 }
 
+Base = declarative_base()
 
-class Client(object):
+
+def client_init(self, **kwargs):
+    for k in kwargs:
+        if isinstance(getattr(self.__class__, k).type, DATE):
+            try:
+                kwargs[k] = datetime.strptime(kwargs[k], "%d.%m.%Y")
+            except ValueError:
+                kwargs[k] = None
+        setattr(self, k, kwargs[k])
+
+
+def create_client(context, row):
     """
-    Desired table class to work with.
-    Will be mapped later from the loaded table
+    creates class instance for row of table
     """
-    def __init__(self,  **kwargs):
-        for k in kwargs:
-            print type(getattr(self, k))
-        exit()
-
-
-def create_client(row_lst):
     kwargs = dict((CSV_DB_MAPPER[CSV_FIELDS[idx]],
-                   el) for idx, el in enumerate(row_lst))
-    return Client(**kwargs)
+                   el) for idx, el in enumerate(row))
+    return context['Client'](**kwargs)
 
 
 def rreplace(s, old, new, occurrence):
+    """
+    replace from the end
+    """
     lst = s.rsplit(old, occurrence)
     return new.join(lst)
 
@@ -63,7 +73,7 @@ def bind(mf, mv, *args, **kwargs):
 
     if not error:
         try:
-            return mf(value, *args, **kwargs)
+            return make_mv(mf(value, *args, **kwargs))
         except Exception as e:
             return make_mv(e="%s: %s" % (mf.__name__, str(e)))
     return mv
@@ -77,32 +87,62 @@ def make_mv(v=None, e=None):
 
 
 def read_config(path):
+    """
+    Reads config file and returns it as a dict
+    """
     with open(path) as f:
         config = json.loads(f.read())
-        return make_mv(config)
+        return config
 
 
 def db_connect(config):
+    """
+    connects to the database, defines table class, creates table if necessary
+    """
     connection = "mysql://%s:%s@localhost/%s" % (config['username'],
                                                  config['password'],
                                                  config['db_name'])
     engine = create_engine(connection)
     config['session'] = sessionmaker(bind=engine)()
-    config['metadata'] = MetaData(engine)
-    t = create_table(config)
-    config['table'] = t
-    return make_mv(config)
+    config['Client'] = type('Client', (Base, ), {
+        'patient_id': Column(Integer),
+        'lastname': Column(VARCHAR(30)),
+        'firstName': Column(VARCHAR(30)),
+        'midname': Column(VARCHAR(30)),
+        'sex': Column(TINYINT(4)),
+        'birthdate': Column(DATE),
+        'doc_series': Column(VARCHAR(8)),
+        'doc_number': Column(VARCHAR(16)),
+        'doc_code': Column(Integer),
+        'policy_series': Column(VARCHAR(16)),
+        'policy_number': Column(VARCHAR(16)),
+        'policy_doctype': Column(Integer),
+        'insurance_orgcode': Column(VARCHAR(12)),
+        'LPU': Column(VARCHAR(12)),
+        'reg_date': Column(DATE),
+        'UPN': Column(BIGINT, primary_key=True),
+        '__tablename__': config['table_name'],
+        '__init__': client_init,
+    })
+    Base.metadata.create_all(engine, checkfirst=True)
+    return config
 
 
 def find_files(context):
+    """
+    Finds all files which fit the *.csv pattern
+    """
     all_csv_files = glob.glob(os.path.join(context['csv_path'],
                                            '*.csv'))
     context['csv_files'] = (c for c in all_csv_files
                             if not c.endswith('_loaded.csv'))
-    return make_mv(context)
+    return context
 
 
 def load_files(context):
+    """
+    Puts all rows of all files into database
+    """
     session = context['session']
     for csv_path in context['csv_files']:
         with open(csv_path) as csv_file:
@@ -113,49 +153,58 @@ def load_files(context):
                 raise ValueError("%s: wrong fields" % csv_path)
 
             for row in reader:
-                c = create_client([x for x in row if x])
+                c = create_client(context, row)
                 session.add(c)
-                session.commit()
-        #os.rename(csv_path, rreplace(csv_path, '.csv', '_loaded.csv', 1))
-    return make_mv(context)
+        try:
+            session.commit()
+            os.rename(csv_path, rreplace(csv_path, '.csv', '_loaded.csv', 1))
+        except Exception as e:
+            session.rollback()
+            raise e
+    return context
 
 
-def load_csv():
+def configure_cron(config):
+    cron = CronTab(config['cron']['user'])
+    command = "python %s %s" % (os.path.realpath(__file__), "csv")
+    print cron.find_command(command)
+    for job in cron.find_command(command):
+        cron.remove(job)
+
+    job = cron.new(command=command)
+    job.minute.every(config['cron']['minute'])
+    job.hour.every(config['cron']['hour'])
+    job.dom.every(config['cron']['day'])
+    job.month.every(config['cron']['month'])
+    job.dow.every(config['cron']['weekday'])
+    job.enable()
+    cron.write()
+
+
+def csv_main():
+    """
+    main function for loading data into database
+    """
     config = bind(read_config, make_mv('config.json'))
     context = bind(db_connect, config)
     context = bind(find_files, context)
     context = bind(load_files, context)
-    return context
+    return context[1] or 'OK'
 
 
-def create_table(context):
-    t = Table(context['table_name'], context['metadata'],
-              Column('patient_id', Integer),
-              Column('lastname', VARCHAR(30)),
-              Column('firstName', VARCHAR(30)),
-              Column('midname', VARCHAR(30)),
-              Column('sex', TINYINT(4)),
-              Column('birthdate', DATETIME),
-              Column('doc_series', VARCHAR(8)),
-              Column('doc_number', VARCHAR(16)),
-              Column('doc_code', Integer),
-              Column('policy_series', VARCHAR(16)),
-              Column('policy_number', VARCHAR(16)),
-              Column('policy_doctype', Integer),
-              Column('insurance_orgcode', VARCHAR(12)),
-              Column('LPU', VARCHAR(12)),
-              Column('reg_date', DATETIME),
-              Column('UPN', Integer, primary_key=True))
-    t.create(checkfirst=True)
-    mapper(Client, t)
-    return t
-
+def cron_main():
+    """
+    main function for configuring cron
+    """
+    config = bind(read_config, make_mv('config.json'))
+    config = bind(configure_cron, config)
+    return config[1] or 'OK'
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('mode', choices=('load',))
+    parser.add_argument('mode', choices=('csv', 'cron'))
     opt = parser.parse_args()
-    if opt.mode == 'load':
-        print load_csv()
-
-
+    if opt.mode == 'csv':
+        print csv_main()
+    elif opt.mode == 'cron':
+        print cron_main()
